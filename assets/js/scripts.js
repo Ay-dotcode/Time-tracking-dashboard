08995f7730acb233965316bc3932ea2706330bad
Ay-dotcode <joseyowolabi@gmail.com>
2024-07-31 00:30:55 +0300

    optimised the code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,69 +1,78 @@
 const main = document.querySelector('main');
-let time, previous, length, datas;
+const day = document.getElementById('day');
+const week = document.getElementById('week');
+const month = document.getElementById('month');
+const datas = [];
+
 getData();
 
 function getData() {
-  if (datas) {
+  if (datas.length > 0) {
     datas.forEach(setData);
   } else {
     fetch("../../data.json")
       .then(response => response.json())
-      .then(datas => {
-        length = datas.length;
+      .then(data => {
+        datas.push(...data);
         datas.forEach(setData);
       })
       .catch(error => console.error(error));
   }
 }
+
 function setData(data) {
+  let id, title, time, previous;
+  id = `${data.title.replace(/\s+/g, '')}`
+  title = data.title;
   if (day.classList.contains('active')) {
     time = data.timeframes.daily.current;
-    previous = `Yesterday - ${data.timeframes.daily.previous}`
+    previous = `Yesterday - ${data.timeframes.daily.previous}`;
   } else if (week.classList.contains('active')) {
     time = data.timeframes.weekly.current;
-    previous = `Last week - ${data.timeframes.weekly.previous}`
+    previous = `Last week - ${data.timeframes.weekly.previous}`;
   } else {
     time = data.timeframes.monthly.current;
-    previous = `Last month - ${data.timeframes.monthly.previous}`
+    previous = `Last month - ${data.timeframes.monthly.previous}`;
   }
-  appendData(time, previous, data);
+  appendData(id, title, time, previous);
 }
-function appendData(time, previous, data) {
-  let sect = document.createElement('section');
-  sect.id = `${data.title.replace(/\s+/g, '')}`
-  sect.innerHTML += `
-      <section class="card">
-        <div class="row">
-          <h3 class="subHeading"> ${data.title}</h3>
-          <img class="options" src="assets/images/icon-ellipsis.svg" alt="options">
-        </div>
-        <div class="row">
-          <p class="Time"> ${time}hrs</p>
-          <p class="Previous">${previous}hrs</p>
-        </div>
-      </section>
-     `
+
+function appendData(id, title, time, previous) {
+  const sect = document.createElement('section');
+  sect.id = id;
+  sect.innerHTML = `
+    <section class="card">
+      <div class="row">
+        <h3 class="subHeading">${title}</h3>
+        <img class="options" src="assets/images/icon-ellipsis.svg" alt="options">
+      </div>
+      <div class="row">
+        <p class="Time">${time}hrs</p>
+        <p class="Previous">${previous}hrs</p>
+      </div>
+    </section>
+  `;
   main.appendChild(sect);
-  while (main.children.length > length + 1) {
+  while (main.children.length > datas.length + 1) {
     main.removeChild(main.children[1]);
   }
-};
-function changeday() {
-  day.classList.add('active');
-  week.classList.remove('active');
-  month.classList.remove('active');
-  getData();
+}
 
-}
-function changeweek() {
-  day.classList.remove('active');
-  week.classList.add('active');
-  month.classList.remove('active');
+function changeActiveTab(event) {
+  const target = event.target;
+  if (target === day) {
+    day.classList.add('active');
+    week.classList.remove('active');
+    month.classList.remove('active');
+  } else if (target === week) {
+    day.classList.remove('active');
+    week.classList.add('active');
+    month.classList.remove('active');
+  } else if (target === month) {
+    day.classList.remove('active');
+    week.classList.remove('active');
+    month.classList.add('active');
+  }
   getData();
 }
-function changemonth() {
-  day.classList.remove('active');
-  week.classList.remove('active');
-  month.classList.add('active');
-  getData();
-}
+document.addEventListener('click', changeActiveTab);
